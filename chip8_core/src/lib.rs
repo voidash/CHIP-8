9fe0dd840bc90f1ecea09e84bb1f9b22b1c91ef4
voidash <ashish.thapa477@gmail.com>
2021-12-29 08:37:21 +0545

    add CPU tick :sparkles: and added pattern matching for Execute

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -84,5 +84,76 @@ impl Emu {
         self.stack[self.sp as usize]
     }
 
+    pub fn reset(&mut self) { 
+        self.pc = START_ADDR;
+        self.ram = [0; RAM_SIZE];
+        self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+        self.v_reg = [0; NUM_REGS];
+        self.i_reg = 0;
+        self.sp = 0;
+        self.stack = [0; STACK_SIZE];
+        self.keys = [false; NUM_KEYS];
+        self.dt = 0;
+        self.st = 0;
+        self.ram[..FONTSET_SIZE].copy_from_slice(&FONT_SET);
+    }
+
+     // Define how CPU will process each instruction and move through the game
+    // 1. fetch value from game
+    // 2. Decode the instruction
+    // 3. Execute, which will modify our CPU registers or RAM.
+    // 4. Move the PC to the next instrction and repeat.
+
+    pub fn tick(&mut self){
+        // Fetch
+        let op = self.fetch();
+        // decoding 
+        // executing
+        self.execute(op);
+    }
+
+    fn fetch(&mut self) -> u16 {
+        //fetch the instruction we are about to execute.
+        let higher_byte = self.ram[self.pc as usize] as u16;
+        let lower_byte = self.ram[(self.pc + 1) as usize] as u16;
+
+
+        // equivalent to adding two bytes to create a 16 bit 
+        // 2f + 4e = 2f4e
+        // 2f is higher byte and 4e is lower byte
+        let op = (higher_byte << 8) | lower_byte;
+        self.pc += 2;
+        op
+    }
+
+    pub fn tick_timers(&mut self) {
+        //every frame decrease the timer by 1;
+
+        // delay timer 
+        if self.dt > 0 {
+            self.dt -= 1;
+        }
+        //sound timer
+        if self.st > 0 {
+            if self.st == 1 {
+                // beep
+            }
+            self.st -= 1;
+        }
+    }
+
+    fn execute(&mut self,op: u16) {
+        // 2f and fe
+
+        // digit 1 is 2 becuase 0010 1111 1111 1110 & 1111 0000 000 0000  = 0010 0000 0000 0000 
+        // shift 0010 0000 0000 0000 by 12. which makes it 0010  
+
+        let digit1 = (op & 0xF000)  >> 12;
+        let digit2 = (op & 0x0F00)  >> 8;
+        let digit3 = (op & 0x00F0)  >> 4;
+        let digit4 = op & 0x000F;
+    
+    }
+
 
 }
